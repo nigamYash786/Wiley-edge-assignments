b95167765eb9aa6c317f568f97af648ba33fa36a
nigam <nigam@DESKTOP-6PCN737>
2022-09-19 10:27:28 +0530

    First Commit

---
+++ Day4/src/Bankacc_question1/BankAccount.java
@@ -0,0 +1,50 @@
+package Bankacc_question1;
+
+public class BankAccount {
+
+	int accountId;
+	String accountHolderName;
+	int openingBalance;
+	double currentBalance;
+	static double totalBalance;
+
+	public BankAccount(int accountId, String accountHolderName, int openingBalance) {
+		this.accountId = accountId;
+		this.accountHolderName = accountHolderName;
+		if (openingBalance < 0)
+			return;
+		this.openingBalance = openingBalance;
+		this.currentBalance = openingBalance;
+		totalBalance += this.currentBalance;
+	}
+
+	public void depositAmount(double amount) {
+		if (amount < 0)
+			return;
+		this.currentBalance += amount;
+		totalBalance += this.currentBalance;
+		System.out.println("Thankyou for depositing Rs." + amount);
+		System.out.println("Current Balance in your account is: Rs." + this.currentBalance);
+		System.out.println("Total available balance in bank is: Rs." + totalBalance);
+	}
+
+	public void withdrawAmount(double amount) {
+		if (amount < 0 || amount > this.currentBalance) {
+			System.out.println("Invalid amount entered.Cannot withdraw");
+			return;
+		}
+		this.currentBalance -= amount;
+		totalBalance -= amount;
+		System.out.println("Thankyou for your service! Your withdrawn amount is Rs." + amount);
+		System.out.println("Current Balance in your account is: Rs." + this.currentBalance);
+		System.out.println("Total available balance in bank is: Rs." + totalBalance);
+	}
+
+	public void getCurrentBalance() {
+		System.out.println("Your current balance is: Rs." + this.currentBalance);
+	}
+
+	public void displayTotalBalance() {
+		System.out.println("Total cash available with bank is: Rs." + totalBalance);
+	}
+}
---
+++ Day4/src/Modify_bankacc_question2/BankAccountType.java
@@ -0,0 +1,13 @@
+package Modify_bankacc_question2;
+
+public enum BankAccountType {
+	
+	SAVINGS(500),CURRENT(1000);
+	int minBalance;
+	private BankAccountType(int minBalance) {
+		this.minBalance = minBalance;
+	}
+	public int getMinBalance() {
+		return this.minBalance;
+	}
+}
